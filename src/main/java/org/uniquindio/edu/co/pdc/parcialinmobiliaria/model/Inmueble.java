581a05ea8bbac70149cccb31cfcabd6b57701b75
SebastianEBR <sebastiane.ballesterosr@uqvirtual.edu.co>
2025-10-17 07:19:34 -0500

    first commit

---
+++ src/main/java/org/uniquindio/edu/co/pdc/parcialinmobiliaria/model/Inmueble.java
@@ -0,0 +1,11 @@
+package org.uniquindio.edu.co.pdc.parcialinmobiliaria.model;
+
+// en estas
+public abstract class Inmueble {
+    protected String ciudad;
+    protected int numHabitaciones;
+    protected int numPisos;
+    protected double precio;
+
+
+}
