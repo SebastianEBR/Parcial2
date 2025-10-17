4011c1ec5567317d8377c8772b55e283d024e52c
SebastianEBR <sebastiane.ballesterosr@uqvirtual.edu.co>
2025-10-17 08:58:15 -0500

    ...

--- src/main/java/org/uniquindio/edu/co/pdc/parcialinmobiliaria/app/HelloApplication.java
+++ src/main/java/org/uniquindio/edu/co/pdc/parcialinmobiliaria/app/HelloApplication.java
@@ -10,7 +10,7 @@ public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws Exception {
         FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource(
-                "/org/uniquindio/edu/co/pdc/parcialinmobiliaria/inmueble-view.fxml"
+                "org/uniquindio/edu/co/pdc/parcialinmobiliaria/vista/InmuebleView.fxml"
         ));
 
         Scene scene = new Scene(fxmlLoader.load(), 750, 500);
